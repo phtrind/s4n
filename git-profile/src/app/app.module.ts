dd0557a27595125528c9e5b065434076bb1993e2
Pedro Trindade <pedro.trindade@dtidigital.com.br>
2019-10-22 19:08:09 -0300

    Adding reference to angular material toolbar.

--- git-profile/src/app/app.module.ts
+++ git-profile/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { ProfileContainerComponent } from './containers/profile-container/profile-container.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileContainerComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // angular-material
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
