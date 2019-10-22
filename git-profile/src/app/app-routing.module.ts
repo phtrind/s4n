5cb28f15cb9ca1c5d3ce5f2751b013b5cc0e93d2
Pedro Trindade <pedro.trindade@dtidigital.com.br>
2019-10-22 19:07:34 -0300

    Adding container to the routing module.

--- git-profile/src/app/app-routing.module.ts
+++ git-profile/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileContainerComponent } from './containers/profile-container/profile-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProfileContainerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
